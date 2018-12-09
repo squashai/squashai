f519dcf0232b0d0e5c4b2960e2abc244ce519a30
Enrico Marocco <enrico.marocco@telecomitalia.it>
2018-12-09 11:55:38 +0100

    Toolbar moved in its own component

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,8 @@
 <template>
   <v-app>
-    <v-navigation-drawer absolute temporary app
-                         v-model="drawer">
-      <v-list>
-        <v-list-tile to="/">
-          <v-list-tile-content>
-            <v-list-tile-title>
-              Home
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile to="/about">
-          <v-list-tile-content>
-            <v-list-tile-title>
-              About
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile
-          href="https://github.com"
-          target="_blank">
-          <v-list-tile-content>
-            <v-list-tile-title>
-              GitHub
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    
-    <v-toolbar app>
-      <v-toolbar-side-icon
-        class="hidden-md-and-up"
-        @click.stop="drawer = !drawer">
-      </v-toolbar-side-icon>
-      <v-toolbar-title class="headline text-uppercase">
-        <v-btn flat
-               class="headline"
-               active-class=""
-               to="/">
-          <span class="font-weight-light">Squash</span>
-          <span>AI</span>
-        </v-btn>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat to="/about">About</v-btn>
-        <v-btn flat
-               href="https://github.com"
-               target="_blank">
-          <span class="mr-2">GITHUB</span>
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-
+    <toolbar
+      :locations="locations">
+    </toolbar>
     <v-content>
       <router-view/>
     </v-content>
@@ -63,10 +12,23 @@
   </v-app>
 </template>
 <script>
+import Toolbar from './components/Toolbar.vue'
+
 export default {
+  name: 'app',
+  components: {
+    Toolbar
+  },
   data () {
     return {
-      drawer: false
+      locations: [{
+        name: 'About',
+        to: '/about'
+      }, {
+        name: 'GitHub',
+        href: 'https://github.com/squashai/squashai',
+        target: '_blank'
+      }]
     }
   }
 }
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      absolute
+      temporary
+      app
+      v-model="drawer">
+      <v-list>
+        <v-list-tile
+          to="/">
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Home
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile
+          v-for="location in locations"
+          :key="location.name"
+          :to="location.to"
+          :href="location.href"
+          :target="location.target">
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{ location.name }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app>
+      <v-toolbar-side-icon
+        class="hidden-md-and-up"
+        @click.stop="drawer = !drawer">
+      </v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <v-btn
+          flat
+          class="headline"
+          active-class=""
+          to="/">
+          <span class="font-weight-light">Squash</span>
+          <span>AI</span>
+        </v-btn>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items
+        class="hidden-sm-and-down"
+        v-for="location in locations"
+        :key="location.name">
+        <v-btn
+          flat
+          :to="location.to"
+          :href="location.href"
+          :target="location.target">
+          {{ location.name }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+<script>
+export default {
+  props: [
+    'locations'
+  ],
+  data() {
+    return {
+      drawer: false
+    }
+  }
+}
+</script>
